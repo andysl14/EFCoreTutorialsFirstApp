726369b056adcf6bc667bce421037b2dab77d872
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2021-02-16 01:09:40 -0400

    Creando la clase contexto

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using EFCoreTutoriaslFirstApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreTutoriaslFirstApp.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Course> Course { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
+    }
+}
