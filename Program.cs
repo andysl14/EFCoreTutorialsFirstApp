7053bbb5a306b16e651b2ed9db3069c9c5ded883
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2021-02-16 00:56:43 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCoreTutoriaslFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
